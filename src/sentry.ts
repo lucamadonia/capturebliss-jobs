a8bcb8e6e46cfefe1b220a4bced518018ce7163c
akash <akash.goswami@unacademy.com>
2023-07-18 05:53:31 +0000

    Scheduled jobs
    carwler-athena job to get all views of a tour
    
    update analytics table
    
    refactoring and query changes
    
    removing unused imports
    
    requested refactoring and minor logic changes
    
    renaming few variables and functions
    
    athena query processing for conversion and ann tour table
    
    adding execute query to return only arrays
    
    enhanced avg calculation query and minor changes
    
    refresh of current and daily data
    
    added case if job table has no success or no data
    
    refactoring of funtions to classes
    
    uncomment necessary commented lines
    
    updated api-contract
    
    modifiying get success data function
    
    organize each classes in seperate files and minor updation in the code
    
    replace failure reason's special character
    
    return all entity from db in one line
    
    sentry integration
    
    sentry for rollup
    
    sentry for cron jobs
    
    corn job timing
    
    change cron job run cycle

---
+++ src/sentry.ts
@@ -0,0 +1,27 @@
+import {init, captureCheckIn} from '@sentry/node';
+
+const DSN_KEY_JOBS = 'SENTRY_DSN_PLACEHOLDER';
+
+export const sentryInitialize = () => {
+  init({
+    dsn: DSN_KEY_JOBS,
+    environment: process.env.JOB_ENV,
+    tracesSampleRate: 1.0,
+  });
+};
+
+export const sentrySuccess = (checkInId: string, jobName: string) => {
+  captureCheckIn({
+    checkInId,
+    monitorSlug: jobName,
+    status: 'ok',
+  });
+};
+
+export const sentryProgress = (jobName: string) => {
+  const checkInId = captureCheckIn({
+    monitorSlug: jobName,
+    status: 'in_progress',
+  });
+  return checkInId;
+};
